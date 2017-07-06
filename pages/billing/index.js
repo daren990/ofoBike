727e26951bc2bb9babc0746fd37cdddfb3904d97
debuggerccai <291898675@qq.com>
2017-07-06 23:08:03 +0800

    新增了立即用车模块

--- pages/billing/index.js
+++ pages/billing/index.js
@@ -1,66 +1,68 @@
-// pages/billing/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    hours: 0,
+    minuters: 0,
+    seconds: 0,
+    billing: "正在计费"
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 页面加载
   onLoad: function (options) {
-  
+    // 获取扫码成功页传过来的车牌号，再定义一个定时器
+    this.setData({
+      number: options.number,
+      timer: this.timer
+    })
+    // 初始化计时器
+    let s = 0;
+    let m = 0;
+    let h = 0;
+    // 计时开始
+    this.timer = setInterval(() => {
+      this.setData({
+        seconds: s++
+      })
+      if (s == 60) {
+        s = 0;
+        m++;
+        setTimeout(() => {
+          this.setData({
+            minuters: m
+          });
+        }, 1000)
+        if (m == 60) {
+          m = 0;
+          h++
+          setTimeout(() => {
+            this.setData({
+              hours: h
+            });
+          }, 1000)
+        }
+      };
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  // 结束骑行，清除定时器
+  endRide: function () {
+    clearInterval(this.timer);
+    this.timer = "";
+    this.setData({
+      billing: "本次骑行耗时",
+      disabled: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 携带定时器状态回到地图
+  moveToIndex: function () {
+    // 如果定时器为空
+    if (this.timer === "") {
+      // 关闭计费页跳到地图
+      wx.redirectTo({
+        url: '../index/index'
+      })
+      // 保留计费页跳到地图，带上计时器状态
+    } else {
+      wx.navigateTo({
+        url: '../index/index?timer=' + this.timer
+      })
+    }
   }
 })
--- pages/scanresult/index.js
+++ pages/scanresult/index.js
@@ -1,66 +1,35 @@
-// pages/scanresult/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    time: 9 // 默认计时时长，这里设短一点，用于调试，ofo app是90s
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // 页面加载
   onLoad: function (options) {
-  
+    // 获取密码
+    this.setData({
+      password: options.password
+    })
+    // 设置初始计时秒数
+    let time = 9;
+    // 开始定时器
+    this.timer = setInterval(() => {
+      this.setData({
+        time: time-- // 倒计时
+      });
+      // 读完秒后携带单车号码跳转到计费页
+      if (time == 0) {
+        clearInterval(this.timer)
+        wx.redirectTo({
+          url: '../billing/index?number=' + options.number
+        })
+      }
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 点击去首页报障
+  moveToWarn: function () {
+    // 清除定时器
+    clearInterval(this.timer)
+    wx.redirectTo({
+      url: '../index/index'
+    })
   }
 })
